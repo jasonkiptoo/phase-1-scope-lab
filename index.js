68037e44af02ab469063205d8b2d982ce60e7b28
jasonkiptoo <kiptoojason@gmail.com>
2022-08-19 09:38:08 +0300

    Completed assignmnent

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+
+var customerName= 'bob'
+
+const leastFavoriteCustomer='Ted'
+
+
+
+
+function upperCaseCustomerName(){
+
+    customerName=customerName.toUpperCase();
+  
+
+}
+//set best customer
+function setBestCustomer(){
+     bestCustomer ='not bob'
+}
+
+
+function overwriteBestCustomer(){
+    bestCustomer='maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+
+     leastFavoriteCustomer="Jayden"
+}
